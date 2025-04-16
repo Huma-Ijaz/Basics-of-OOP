43e0017555ddd37090fe6752ec9ead2629ae689f
Huma Ijaz <humaijaz193@gmail.com>
2025-04-17 02:49:42 +0500

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,32 @@
+class Account{
+    private double balance;
+
+    Account(){
+        balance = 0;
+    }
+    Account(double b){
+        balance = b;
+    }
+
+    void depositBalance(double new_balance){
+        balance += new_balance;
+        System.out.println("Deposited amount: " + new_balance + "\n" + "Current BALANCE: " + balance);
+    }
+
+    void withdrawal(double newbalance){
+        if(balance >= newbalance){
+            balance -= newbalance;
+            System.out.println("Withdrawl Amount: " + newbalance + "\n" + "Current Balance: " + balance );
+        }
+    }
+}
+
+
+public class Bank {
+    public static void main(String[] args) {
+        Account b1 = new Account(50000);
+        b1.depositBalance(2000);
+
+
+    }
+}
