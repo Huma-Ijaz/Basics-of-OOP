280e77870e097118544c88e85cc1ad5e5ae482af
Huma Ijaz <humaijaz193@gmail.com>
2025-04-22 05:45:52 +0500

    Add files via upload

---
+++ s1p.java
@@ -0,0 +1,47 @@
+
+    class book{
+        private String name;
+        private String author_name;
+        private int price;
+
+        book(String n, String a, int p){
+            name = n;
+            author_name = a;
+            price = p;
+        }
+
+        book(book obj){
+
+            this.name = obj.name;
+            this.author_name = obj.author_name;
+            this.price = obj.price;
+        }
+
+        void display(){
+            System.out.println("Book: " + name);
+            System.out.println("Author: " + author_name);
+            System.out.println("Price: " + price);
+        }
+
+        void setName(String na){
+            name = na;
+        }
+    }
+
+
+    public class s1p {
+        public static void main(String[] args) {
+            book oop = new book("Java","HSY",5000);
+            oop.display();
+
+            book oop2 = new book(oop);
+            oop2.setName("PF");
+            oop2.display();
+
+//        book oop3 = new book(oop,pf);
+//        oop3.display();
+
+        }
+    }
+
+
